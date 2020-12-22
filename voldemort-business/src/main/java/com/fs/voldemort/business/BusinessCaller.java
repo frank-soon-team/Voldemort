c688ade92f30ea40e01feba22b6b669340a9c4dc
frank <zhumingxu666@outlook.com>
2020-12-22 16:16:15 +0800

    提交business代码

--- voldemort-business/src/main/java/com/fs/voldemort/business/BusinessCaller.java
+++ voldemort-business/src/main/java/com/fs/voldemort/business/BusinessCaller.java
@@ -1,5 +1,6 @@
 package com.fs.voldemort.business;
 
+import com.fs.voldemort.business.BusinessFuncRegistry.BusinessFunc;
 import com.fs.voldemort.business.support.BusinessFuncCallable;
 import com.fs.voldemort.core.Caller;
 import com.fs.voldemort.core.support.CallerParameter;
@@ -17,19 +18,16 @@ public class BusinessCaller extends Caller {
         return new BusinessCaller();
     }
 
+    private BusinessCaller() {
+        super();
+    }
+
     public BusinessCaller call(Class<BusinessFuncCallable> funcClazz) {
-
-        // this.call(p -> null);
-
-        // 1. 获取bean businessFuncRegistry.get(funcClazz);
-        // 2. 找到要执行的函数 Method
-        // 3. 得到Method参数列表
-
-        // 4. 准备参数，如果bean instanceof BusinessFuncCallable 调用paramFit，否则报错
-        // 5. ParameterList<Args>.invoke(method);
-
+        final BusinessFunc bFunc = businessFuncRegistry.getFunc(funcClazz);
+        this.call(p->{
+            return bFunc.func.call(bFunc.paramFitFunc.call(p).toArray());
+        });
         return this;
-
     }
 
     @Override
--- voldemort-business/src/main/java/com/fs/voldemort/business/BusinessFuncRegistry.java
+++ voldemort-business/src/main/java/com/fs/voldemort/business/BusinessFuncRegistry.java
@@ -1,8 +1,10 @@
 package com.fs.voldemort.business;
 
+import com.fs.voldemort.business.support.BusinessFuncCallable;
 import com.fs.voldemort.business.support.BusinessFuncHorcruxes;
 import com.fs.voldemort.business.support.BusinessFuncMark;
 import com.fs.voldemort.core.exception.CallerException;
+import com.fs.voldemort.core.support.CallerParameter;
 import com.fs.voldemort.func.DynamicFunc;
 import com.fs.voldemort.func.Func;
 
@@ -11,6 +13,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -19,50 +22,77 @@ import java.util.stream.Collectors;
  */
 public class BusinessFuncRegistry {
 
-    private Map<Class, DynamicFunc> funcRegistry ;
+    private Map<Class, BusinessFunc> funcContainer;
 
-    private final Func<Class, Map<String, Object>> getBusinessFuncHorcruxesFunc;
+    private final Func<Class, Map<String, BusinessFuncCallable>> getBusinessFuncHorcruxesFunc;
 
-    public BusinessFuncRegistry(Func<Class, Map<String, Object>> getBusinessFuncHorcruxesFunc){
+    public BusinessFuncRegistry(Func<Class, Map<String, BusinessFuncCallable>> getBusinessFuncHorcruxesFunc){
         this.getBusinessFuncHorcruxesFunc = getBusinessFuncHorcruxesFunc;
         init();
         scanAndFill();
     }
 
     private void init(){
-        funcRegistry = new ConcurrentHashMap<>();
+        funcContainer = new ConcurrentHashMap<>();
         BusinessCaller.businessFuncRegistry = this;
     }
 
     private void scanAndFill() {
-        final Map<String, Object> funcHorcruxesBeanMap = getBusinessFuncHorcruxesFunc.call(BusinessFuncHorcruxes.class);
+        final Map<String, BusinessFuncCallable> funcHorcruxesBeanMap = getBusinessFuncHorcruxesFunc.call(BusinessFuncHorcruxes.class);
         if (funcHorcruxesBeanMap.isEmpty()) {
             return;
         }
-        final Map<Method, Object> assistFuncHorcruxesInstanceMap = new HashMap<>(funcRegistry.size());
-        funcRegistry.putAll(funcHorcruxesBeanMap.values().stream()
-                .map(funcHorcruxes -> Arrays.stream(funcHorcruxes.getClass().getDeclaredMethods())
-                        .filter(method -> method.isAnnotationPresent(BusinessFuncMark.class))
-                        .peek(method -> assistFuncHorcruxesInstanceMap.put(method, funcHorcruxes))
-                        .collect(Collectors.toList()))
+        final Map<Method, BusinessFuncCallable> assistFuncHorcruxesInstanceMap = new HashMap<>(funcContainer.size());
+        funcContainer.putAll(funcHorcruxesBeanMap.values().stream()
+                .map(
+                    funcHorcruxes -> {
+                        return Arrays.stream(funcHorcruxes.getClass().getDeclaredMethods())
+                            .filter(method -> method.isAnnotationPresent(BusinessFuncMark.class))
+                            .peek(method -> assistFuncHorcruxesInstanceMap.put(method, funcHorcruxes))
+                            .collect(Collectors.toList());
+                    }
+                )
                 .flatMap(Collection::stream)
-                .collect(Collectors.toMap(
-                        method -> method.getAnnotation(BusinessFuncMark.class).value(),
-                        method -> args -> {
-                                try {
-                                    return method.invoke(assistFuncHorcruxesInstanceMap.get(method), args);
-                                } catch (Exception e) {
-                                    throw new CallerException(e.getMessage());
-                                }
-                            }
-                        )
+                .collect(
+                    Collectors.toMap(
+                        method -> method.getClass(),
+                        method -> {
+                            return new BusinessFunc(
+                                assistFuncHorcruxesInstanceMap.get(method).getClass(), 
+                                args -> {
+                                    try {
+                                        return method.invoke(assistFuncHorcruxesInstanceMap.get(method),args);
+                                    } catch (Exception e) {
+                                        throw new CallerException(e.getMessage());
+                                    }
+                                },
+                                p -> assistFuncHorcruxesInstanceMap.get(method).paramFit(p)
+                            );
+                        }
+                    )
                 )
         );
     }
 
     @SuppressWarnings("unchecked")
-    public <T> DynamicFunc<T> getFunc(final Class funcClazz, Class<T> resultClazz) {
-        return funcRegistry.get(funcClazz);
+    public BusinessFunc getFunc(final Class funcClazz) {
+        return funcContainer.get(funcClazz);
+    }
+
+    public class BusinessFunc {
+
+        public final Class funcClazz;
+
+        public final DynamicFunc func;
+
+        public final Func<CallerParameter,Set<BusinessFuncCallable.Args>> paramFitFunc;
+
+        public BusinessFunc(Class funClass, DynamicFunc func, 
+            Func<CallerParameter,Set<BusinessFuncCallable.Args>> paramFitFunc) {
+            this.funcClazz = funClass;
+            this.func = func;
+            this.paramFitFunc = paramFitFunc;
+        }
     }
 
 }
