43350dfae8ccca6bf0bfa707a2ed837214edda1a
soonsoft <soonz@outlook.com>
2021-05-20 11:38:02 +0800

    完成示例应用程序框架搭建

---
+++ voldemort-example/voldemort-example.uranus/src/main/java/com/soonsoft/uranus/site/SiteApplication.java
@@ -0,0 +1,34 @@
+package com.soonsoft.uranus.site;
+
+import com.soonsoft.uranus.security.simple.EnableSimpleSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.soonsoft.uranus.site.config",
+        "com.soonsoft.uranus.site.controller",
+        "com.fs.voldemort.example.service"
+})
+@EnableSimpleSecurity
+public class SiteApplication {
+
+    public static void main(String[] args) {
+        onStop();
+        SpringApplication.run(SiteApplication.class, args);
+    }
+
+    private static void onStop() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+               System.out.println("SiteApplication Shutdown.");
+            }
+        });
+    }
+    
+}
