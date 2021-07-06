e7ee2ee7343cab526106ff69003649312c943db5
soonsoft <soonz@outlook.com>
2021-07-06 13:14:23 +0800

    代码清扫
    去掉没用的依赖

--- voldemort-springboot-autoconfigure/src/main/java/com/fs/voldemort/spi/VoldemortAutoConfiguration.java
+++ voldemort-springboot-autoconfigure/src/main/java/com/fs/voldemort/spi/VoldemortAutoConfiguration.java
@@ -1,13 +1,11 @@
 package com.fs.voldemort.spi;
 
 import com.fs.voldemort.business.BFuncAvailableCaller;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class VoldemortAutoConfiguration implements ApplicationContextAware {
 
